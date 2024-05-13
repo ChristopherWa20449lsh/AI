534adbb8695b903789f414ca9692579b060c5c4a
GG <qq1270414897@gmail.com>
2024-05-13 15:33:00 +0800

    忽略大文件追踪

---
+++ ignore.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# find . -type f -size +100M | while read -r file
+# do
+#     # Add them to .gitignore
+#     echo "$file" >> .gitignore
+# done
+
+# 注意-print必须显式指定
+find . -path ./.git -prune -o -type f -size +50M -print | sed -e 's/\.\///g' -e 's/ /\\ /g' > .gitignore
